1be684b6ac42a0208498c9d1964920e3938cb46f
kapil551 <59477738+kapil551@users.noreply.github.com>
2021-12-17 15:35:09 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -11,16 +11,17 @@ function Header() {
 
         <div className="text-pink-500 sticky top-0 p-5 z-50 bg-black shadow-sm border-b-2 border-pink-700">
            
-           <div className="grid grid-cols-5 lg:grid-cols-6 items-end lg:items-center">
+           <div className="grid grid-cols-4 lg:grid-cols-4 items-end lg:items-center">
 
-               <div className="relative h-24 w-24 mx-auto hidden lg:inline-grid">
+               {/* <div className="relative h-24 w-24 mx-auto hidden lg:inline-grid">
                    <Image 
-                    src="https://links.papareact.com/3pi"
+                    src=""
+                    alt="my-pic"
                     layout="fill"
                     objectFit="cover"
                     className="rounded-full"
                    />
-               </div>
+               </div> */}
 
                <div className="text-left lg:text-center col-span-4">
                    <div className="h-48 w-48 relative lg:mx-auto border-pink-500 border-8 rounded-full">
